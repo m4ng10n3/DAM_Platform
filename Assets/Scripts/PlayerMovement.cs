51b18497849c955332e1e75ff93ccd6763bbb79d
m4ng10n3 <gianlucamangione95@gmail.com>
2025-11-02 20:28:43 +0100

    ground check and wall check change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerMovement : MonoBehaviour
@@ -14,16 +15,31 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Ground Check")]
     public Transform groundCheckTransform;
-    public Vector2 groundCheckSize = new Vector2(0.5f, 0.1f);
     public LayerMask groundLayer;
+    public Tilemap groundTilemap;
+    bool isGrounded;    
+    
+    [Header("Wall Check")]
+    public Transform wallCheckTransform;
+    bool isOnWall;
+
+    [Header("Wall Jump")]
+    public float wallJumpPush = 10f;            
+    public float wallJumpPushDuration = 0.1f; // quanto dura la spinta
+    float wallJumpPushTimer = 0f;
+    float wallJumpDir = 0f;                    // -1 sinistra, +1 destra
+
+    [Header("SFX")]
+    public AudioClip JumpSFX;
 
     [Header("Components")]
     public Animator playerAnimator;
     public SpriteRenderer spriteRenderer;
     Rigidbody2D body;
-    bool isGrounded;
+    
     bool facingRight = true;
-    Vector3 scale0;
+    public AudioSource audioSource;
+    public float wallspan = 0.71f;
 
     float horizontalMovement = 0;
 
@@ -34,8 +50,24 @@ public class PlayerMovement : MonoBehaviour
 
     public void FixedUpdate()
     {
-        body.linearVelocityX = horizontalMovement * playerSpeed;
+        if (wallJumpPushTimer > 0f && horizontalMovement==0)
+        {
+            body.linearVelocityX = wallJumpDir * wallJumpPush;
+            wallJumpPushTimer -= Time.fixedDeltaTime;
+            /*
+            Debug.Log("Timer");
+            Debug.Log(wallJumpPushTimer);
+            Debug.Log("wallJumpDir");
+            Debug.Log(wallJumpDir);
+            */
+        }
+        else
+        {
+            body.linearVelocityX = horizontalMovement * playerSpeed;
+            wallJumpPushTimer = 0f;
+        }
         GroundCheck();
+        WallCheck();
         SetGravity();
     }
 
@@ -44,6 +76,7 @@ public class PlayerMovement : MonoBehaviour
         playerAnimator.SetFloat("XSpeed", Mathf.Abs(body.linearVelocityX));
         playerAnimator.SetFloat("YSpeed", body.linearVelocityY);
         playerAnimator.SetBool("isGrounded", isGrounded);
+        playerAnimator.SetBool("isOnWall", isOnWall);
 
         if (horizontalMovement > 0 && !facingRight)
         {
@@ -57,7 +90,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void SetGravity()
     {
-        if(body.linearVelocityY < 0)
+        if (body.linearVelocityY < 0)
         {
             body.gravityScale = baseGravity * fallSpeedMultiplier;
             body.linearVelocityY = Mathf.Max(body.linearVelocityY, -maxFallSpeed);
@@ -68,16 +101,28 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-
     public void GroundCheck()
     {
-        if (Physics2D.OverlapBox(groundCheckTransform.position, groundCheckSize, 0, groundLayer))
-            isGrounded = true;
-        else
-            isGrounded = false;
+        // Punto da controllare (leggermente sotto i piedi)
+        Vector3 wp = groundCheckTransform.position;
 
-        
+        Vector3Int cell = groundTilemap.WorldToCell(wp);
+
+        // Se la cella contiene un tile, il punto è "dentro il terreno"
+        isGrounded = groundTilemap.HasTile(cell);
     }
+
+    public void WallCheck()
+    {
+        // Punto da controllare (leggermente sotto i piedi)
+        Vector3 wp = wallCheckTransform.position;
+
+        Vector3Int cell = groundTilemap.WorldToCell(wp);
+
+        // Se la cella contiene un tile, il punto è "dentro il terreno"
+        isOnWall = groundTilemap.HasTile(cell);
+    }
+
     public void PlayerInput_Move(CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
@@ -85,14 +130,26 @@ public class PlayerMovement : MonoBehaviour
 
     public void PlayerInput_Jump(CallbackContext context)
     {
-     
+
         if (isGrounded)
         {
 
-             if (context.performed)
+            if (context.performed)
             {
                 body.linearVelocityY = jumpForce;
-            }      
+                audioSource.PlayOneShot(JumpSFX);
+            }
+        }
+        else if (isOnWall)
+        {
+            if (context.performed)
+            {
+                body.linearVelocityY = jumpForce;
+                wallJumpDir = facingRight ? -1 : 1;
+                wallJumpPushTimer = wallJumpPushDuration;
+                Flip();
+                audioSource.PlayOneShot(JumpSFX);
+            }
         }
 
         if (context.canceled && body.linearVelocityY > 0)
@@ -104,13 +161,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void Flip()
     {
+        Vector3 pos = wallCheckTransform.localPosition;
+        if (facingRight)
+        {
+            pos.x -= wallspan;
+        }
+        else
+        {
+            pos.x += wallspan;
+        }
         facingRight = !facingRight;
         spriteRenderer.flipX = !spriteRenderer.flipX;
+        wallCheckTransform.localPosition = pos;
     }
 
     public void OnDrawGizmos()
     {
-        Gizmos.DrawCube(groundCheckTransform.position, groundCheckSize);
+        //Gizmos.DrawCube(groundCheckTransform.position, groundCheckSize);
+        //Gizmos.DrawCube(wallCheckTransform.position, wallCheckSize);
+        Vector3 g = groundCheckTransform.position;
+        Gizmos.DrawWireSphere(g, 0.03f);
+
+        Vector3 w = wallCheckTransform.position;
+        Gizmos.DrawWireSphere(w, 0.03f);
     }
 
 
